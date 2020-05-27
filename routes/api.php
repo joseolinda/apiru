bf3fcd6f8f78c3c666fdde45d6673cd4a8fb5db2
João Denílson Souza dos Santos <denilsonsantos021@gmail.com>
2020-05-27 11:28:36 -0300

    Permintindo a recepçaõ visualizar os relatorios de refeições

--- routes/api.php
+++ routes/api.php
@@ -132,7 +132,7 @@ route::group(['prefix'=>'menu','middleware' => ['check.admin','check.reception',
     Route::delete('/{id}', 'Nutritionist\MenuController@destroy')->name('menu.destroy');
 });
 
-route::group(['prefix'=>'report','middleware' => ['check.admin','check.reception']],function (){
+route::group(['prefix'=>'report','middleware' => ['check.admin']],function (){
     route::get('/list-scheduling','ReportStudentMealController@listScheduling')->name('report.listScheduling');
     route::get('/all-meal','ReportStudentMealController@allMeal')->name('report.allMeal');
 });
