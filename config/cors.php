91e532e2263ed767cc7b267c7352e92947258320
plsb <peluissaraiva@gmail.com>
2021-04-15 11:34:01 -0300

    - condigurado a política de CORS para liberar conexão no local host

--- config/cors.php
+++ config/cors.php
@@ -21,40 +21,42 @@ return [
      * You can enable CORS for 1 or multiple paths.
      * Example: ['api/*']
      */
-    'paths' => [],
+    'paths' => ['api/*'],
 
     /*
     * Matches the request method. `[*]` allows all methods.
     */
-    'allowed_methods' => ['*'],
+    'allowed_methods' => ['GET, POST, PUT, DELETE, OPTIONS'],
 
     /*
-     * Matches the request origin. `[*]` allows all origins. Wildcards can be used, eg `*.mydomain.com`
+     * Matches the request origin. `[*]` allows all origins. Wildcards can be u$
      */
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['http://localhost:3000'],
 
     /*
      * Patterns that can be used with `preg_match` to match the origin.
      */
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => ['Content-Type',
+        'Origin','Authorization'],
 
     /*
-     * Sets the Access-Control-Allow-Headers response header. `[*]` allows all headers.
+     * Sets the Access-Control-Allow-Headers response header. `[*]` allows all $
      */
     'allowed_headers' => ['*'],
 
     /*
-     * Sets the Access-Control-Expose-Headers response header with these headers.
+     * Sets the Access-Control-Expose-Headers response header with these header$
      */
-    'exposed_headers' => [],
+    'exposed_headers' => ['Authorization'],
 
     /*
      * Sets the Access-Control-Max-Age response header when > 0.
      */
-    'max_age' => 0,
+    'max_age' => 3600,
 
     /*
      * Sets the Access-Control-Allow-Credentials header.
      */
     'supports_credentials' => false,
+
 ];
