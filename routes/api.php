5cf9f2b8ec1d9fc612048dd403e12b5d76e7f6d3
Jose Olinda <joseolindad@yahoo.com.br>
2022-08-08 17:29:13 -0300

    Rotas de Respostas do formulário

--- routes/api.php
+++ routes/api.php
@@ -189,7 +189,7 @@ route::group(['prefix' => 'forms', 'middleware' => ['check.admin', 'check.assist
 });
 
 //Respostas do formulário
-route::group(['prefix'=> 'student/forms', 'middleware' => ['check.admin', 'check.student'] function(){
+route::group(['prefix'=> 'student/forms', 'middleware' => ['check.admin', 'check.student']], function(){
     route::get('/', 'Forms\Student@index')->name('forms.view');
     route::post('/response', 'Forms\Student@store')->name('forms.response');    
-}])
+});
