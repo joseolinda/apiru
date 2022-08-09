c345d5931881844a65a84369f516768f7f20d915
joseolinda <joseolindad@yahoo.com.br>
2022-08-09 06:48:13 -0300

    Estruturando os controllers na parta Forms

--- app/Http/Controllers/Forms/FormularioController.php
+++ app/Http/Controllers/Forms/FormularioController.php
@@ -1,8 +1,9 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Forms;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class FormularioController extends Controller
 {
